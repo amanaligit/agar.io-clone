9f5d48c361934337b994edc018f738d3c76e533d
amanaligit <ali.aman.2010@gmail.com>
2021-03-20 15:24:22 +0530

    changed port back to 8080

--- servers.js
+++ servers.js
@@ -1,21 +1,12 @@
 const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-
 const app = express();
-app.use(cors());
-app.use(helmet());
 app.use(express.static(__dirname + "/public"));
 const socketio = require('socket.io');
-const expressServer = app.listen(80, '0.0.0.0');
-const io = socketio(expressServer, {
-    cors: {
-        origin: '*',
-    }
-});
-
-
-console.log("Express and socketIO are listening on port 80");
+const expressServer = app.listen(8080, '0.0.0.0');
+const io = socketio(expressServer);
+const helmet = require('helmet');
+app.use(helmet());
+console.log("Express and socketIO are listening on port 8080");
 
 module.exports = {
     app, io
