7c45c767652fb831824f45264553c22f3d3bd562
amanaligit <ali.aman.2010@gmail.com>
2021-03-25 10:29:29 +0530

    changed to env port for deployment

--- servers.js
+++ servers.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname + "/public"));
 const socketio = require('socket.io');
-const expressServer = app.listen(8080, '0.0.0.0');
+const PORT = process.env.PORT || '5000'
+const expressServer = app.listen(PORT, () => console.log("Express and socketIO are listening on port ", PORT));
 const io = socketio(expressServer);
 const helmet = require('helmet');
 app.use(helmet());
-console.log("Express and socketIO are listening on port 8080");
-
 module.exports = {
     app, io
 }
