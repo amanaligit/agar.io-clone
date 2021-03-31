3735ccb0ac06e2ad6fe086075a80886a69932acc
amanaligit <ali.aman.2010@gmail.com>
2021-03-31 15:11:49 +0530

    fixed bug for env vars

--- gameSettings.js
+++ gameSettings.js
@@ -2,10 +2,10 @@
 //DEFAULT game settings, can be modified from heroku;
 
 module.exports = {
-    defaultOrbs: process.env.DEFAULT_ORBS || 200,
-    defaultSpeed: process.env.DEFAULT_SPEED || 3,
-    defaultSize: process.env.DEFAULT_SIZE || 6,
-    defaultZoom: process.env.DEFAULT_ZOOM || 2,
-    worldWidth: process.env.WORLD_WIDTH || 1000,
-    worldHeight: process.env.WORLD_HEIGHT || 1000
+    defaultOrbs: parseInt(process.env.DEFAULT_ORBS) || 200,
+    defaultSpeed: parseInt(process.env.DEFAULT_SPEED) || 3,
+    defaultSize: parseInt(process.env.DEFAULT_SIZE) || 6,
+    defaultZoom: parseInt(process.env.DEFAULT_ZOOM) || 2,
+    worldWidth: parseInt(process.env.WORLD_WIDTH) || 1000,
+    worldHeight: parseInt(process.env.WORLD_HEIGHT) || 1000
 }
